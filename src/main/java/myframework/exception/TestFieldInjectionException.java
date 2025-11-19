06ab2295ee327f80b75d579fe6e55c91d66f190b
Yeji Kim <yejikim.erica@gmail.com>
2025-11-19 23:51:16 +0900

    feat(myframework): 단위 테스트용 필드 주입 기능 추가
    클라이언트의 테스트 편의를 위한 유틸리티 클래스를 구현함.
    - @Autowired 필드 주입 방식을 사용하는 클래스의 단위 테스트 지원
    - Java Reflection API를 활용해 private 필드에 강제 주입하는 inject 메서드 제공
    - 예외 처리 및 검증을 위한 TestUtilsTest 작성

---
+++ src/main/java/myframework/exception/TestFieldInjectionException.java
@@ -0,0 +1,7 @@
+package myframework.exception;
+
+public class TestFieldInjectionException extends RuntimeException {
+    public TestFieldInjectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
