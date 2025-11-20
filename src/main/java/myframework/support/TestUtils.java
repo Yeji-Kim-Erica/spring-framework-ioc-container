20c1ff046286b1920be787e939111d517759cfa6
Yeji Kim <yejikim.erica@gmail.com>
2025-11-20 22:34:18 +0900

    feat(myframework): 단위 테스트 의존성 주입 기능 확장
    상속된 필드에 대한 주입 지원 추가
    - 부모 클래스의 필드까지 탐색 범위를 확장하여 inject 기능 강화
    - 관련 테스트 리팩토링 및 상속 케이스 검증 추가

--- src/main/java/myframework/support/TestUtils.java
+++ src/main/java/myframework/support/TestUtils.java
@@ -4,6 +4,7 @@ import myframework.exception.ErrorMessage;
 import myframework.exception.TestFieldInjectionException;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 /**
  * 테스트 편의를 위한 유틸리티 클래스
@@ -13,11 +14,23 @@ public final class TestUtils {
 
     public static void inject(Object target, String fieldName, Object dependency) {
         try {
-            Field field = target.getClass().getDeclaredField(fieldName);
+            Field field = findFieldToInject(target, fieldName);
             field.setAccessible(true);
             field.set(target, dependency);
         } catch (Exception e) {
             throw new TestFieldInjectionException(ErrorMessage.TEST_FIELD_INJECTION_FAILED.getMessage(fieldName), e);
         }
+    }
+
+    private static Field findFieldToInject(Object target, String fieldName) throws NoSuchFieldException {
+        Class<?> clazz = target.getClass();
+        while (clazz != null && clazz != Object.class) {
+            try {
+                return clazz.getDeclaredField(fieldName);
+            } catch (NoSuchFieldException e) {
+                clazz = clazz.getSuperclass();
+            }
+        }
+        throw new NoSuchFieldException();
     }
 }
--- src/test/java/myframework/support/TestUtilsTest.java
+++ src/test/java/myframework/support/TestUtilsTest.java
@@ -5,35 +5,59 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import myframework.exception.TestFieldInjectionException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class TestUtilsTest {
     static class TestTarget {
         private String field;
     }
-    
-    @DisplayName("private 필드에 값을 강제로 주입한다")
-    @Test
-    void shouldSetPrivateField() {
-        // given
-        TestTarget target = new TestTarget();
-        String value = "dependency";
 
-        // when
-        TestUtils.inject(target, "field", value);
-
-        // then
-        assertThat(target).extracting("field").isEqualTo(value);
+    static class TestInheritedTarget extends TestTarget {
     }
 
-    @DisplayName("존재하지 않는 필드에 주입하려고 하면 예외가 발생한다")
-    @Test
-    void throwException_WhenFieldnotExists() {
-        // given
-        TestTarget target = new TestTarget();
+    @Nested
+    class SuccessTest {
+        @DisplayName("private 필드에 값을 강제로 주입한다")
+        @Test
+        void shouldSetPrivateField() {
+            // given
+            TestTarget target = new TestTarget();
+            String value = "dependency";
 
-        // when & then
-        assertThatThrownBy(() -> TestUtils.inject(target, "wrongField", "value"))
-                .isInstanceOf(TestFieldInjectionException.class);
+            // when
+            TestUtils.inject(target, "field", value);
+
+            // then
+            assertThat(target).extracting("field").isEqualTo(value);
+        }
+
+        @DisplayName("부모에게 물려받은 필드에 값을 강제로 주입한다")
+        @Test
+        void shouldSetInheritedField() {
+            // given
+            TestInheritedTarget target = new TestInheritedTarget();
+            String value = "dependency";
+
+            // when
+            TestUtils.inject(target, "field", value);
+
+            // then
+            assertThat(target).extracting("field").isEqualTo(value);
+        }
+    }
+
+    @Nested
+    class ExceptionTest {
+        @DisplayName("존재하지 않는 필드에 주입하려고 하면 예외가 발생한다")
+        @Test
+        void throwException_WhenFieldnotExists() {
+            // given
+            TestTarget target = new TestTarget();
+
+            // when & then
+            assertThatThrownBy(() -> TestUtils.inject(target, "wrongField", "value"))
+                    .isInstanceOf(TestFieldInjectionException.class);
+        }
     }
 }
